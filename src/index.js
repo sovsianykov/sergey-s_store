99919a2145f6a9d3d96d79da74f9e9e83a02c988
”sovsianykov” <swebproger@gmail.com>
2022-01-22 11:39:36 +0200

    final-commit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
 import { setupStore } from "./redux/store";
 const store = setupStore();
 
-
 ReactDOM.render(
-  <React.StrictMode>
-      <Provider store={store}>
+  <Provider store={store}>
     <App />
-      </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
